31d68d59bae79c182abdd5f704e85c8a85a5cb93
Brian Morrow <bmorrow92@gmail.com>
2012-04-09 23:14:59 -0400

    updated

--- game_objects.py
+++ game_objects.py
@@ -20,7 +20,10 @@ class HeadsUpDisplay:
 		self.headsup = pygame.image.load(HUD_image).convert_alpha()
 		self.angle = 0
 	
-	def draw(self, screen, angle): 
+	def drawheadsup(self, screen):
+		screen.blit(self.headsup, (0,0))
+
+	def drawcircle(self, screen, angle): 
 		screen.blit(self.headsup, (0,0))
 		circlerect = pygame.Rect(0,0,165,165)
 		circlerect2 = pygame.Rect(0,0,170,170)
@@ -32,13 +35,23 @@ class HeadsUpDisplay:
 		y = 82*math.cos(angle)+712
 		pygame.draw.line(screen, (0,255,0), (640, 712), (x, y), 2)
 
+	def drawHealth(self, screen, health):
+		text = "Health"
+		font = pygame.font.Font('freesansbold.ttf', 14)
+		surf = font.render(text, True, (255,255,255))
+		screen.blit(surf, (205, 729))
+		barRect = pygame.Rect(200, 745, 250, 50)
+		healthRect = pygame.Rect(210, 755, health - 20, 30)
+		pygame.draw.rect(screen, (0,0,0), barRect)
+		pygame.draw.rect(screen, (255,0,0), healthRect)
+
+
 	def updateangle(self, angle):
 		angle += .05
 		pi = 3.141592653
 		if angle > 2*pi:
 			angle = angle - 2*pi
 		return angle
-
 
 class Background():
 	def __init__(self):
@@ -47,21 +60,13 @@ class Background():
 		self.background_y = 0
 		self.background_x = 0
 
-
-	def draw_rotated(self, screen, x, y, score):
-		scoreText = 'Score: ' 
-		screenFont = pygame.font.Font('freesansbold.ttf', 14)
-		screenSurf = screenFont.render('Press F for fullscreen.', True, (1,1,1))
-		scoreSurf = screenFont.render('Score: '+ str(score), True, (1,1,1))
-		screen.blit(self.background, (x, y))
-		screen.blit(screenSurf, (1000, 14))
-		screen.blit(scoreSurf, (1000, 30))
-
-	def checkbounds(self):
+	def checkbounds(self, screen, x, y):
 		if self.background_x <-1280:
 			self.background_x = 0
 		if self.background_x >0:
 			self.background_x = -1280
+		screen.blit(self.background, (x, y))
+
 class Enemy:
 	def __init__(self):
 		self.enemy = pygame.image.load(enemy_image).convert_alpha()
@@ -74,10 +79,19 @@ class Enemy:
 		self.health = randint(5, 15)
 
 	def draw(self, screen, (x, y), sizex, sizey):
-		if sizex >= 200:
-			sizex = 200
-			sizey = 200
+		if sizex >= 100:
+			self.enemy = pygame.transform.scale(pygame.image.load(enemy_image).convert_alpha(), (100, 100))
+
+		if sizex >= 300:
+			self.enemy = pygame.transform.scale(pygame.image.load(enemy_image).convert_alpha(), (300, 300))
+			#sizex = 300
+			#sizey = 300
+
+		if sizex >= 450:
+			sizex = 450
+			sizey = 450
 		scaledenemy = pygame.transform.scale(self.enemy, (sizex, sizey))
+		x -= sizex/2
 		screen.blit(scaledenemy, (x, y))
 		rect = pygame.Rect(x, y, sizex, sizey)
 		return rect
