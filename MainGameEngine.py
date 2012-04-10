6aa4dd65214da78dbf5dfd680a609025afda0c73
Brian Morrow <bmorrow92@gmail.com>
2012-04-09 23:15:31 -0400

    updated enemy behavior/added player health

--- MainGameEngine.py
+++ MainGameEngine.py
@@ -8,12 +8,19 @@ from sys import exit
 from math import *
 from MenuScreen import *
 
-
 def main():
 	pygame.init()
 	showstartscreen()
 	while True:
 		runGame()
+
+def draw_score(screen, score):
+	scoreText = 'Score: ' 
+	screenFont = pygame.font.Font('freesansbold.ttf', 14)
+	screenSurf = screenFont.render('Press F for fullscreen', True, (255,255,255))
+	scoreSurf = screenFont.render('Score: '+ str(score), True, (255,255,255))
+	screen.blit(screenSurf, (1000, 756))
+	screen.blit(scoreSurf, (1000, 770))
 
 def runGame():
 	pygame.init()
@@ -23,7 +30,7 @@ def runGame():
 	bullets = []
 	for x in range(50):
 		bullets.append(bullet)
-	bulletposy = SCREEN_SIZE[1]
+	bulletposy = SCREEN_SIZE[1] -100
 	bulletsizex = 20
 	bulletsizey = 20
 	headsupdisplay = HeadsUpDisplay()
@@ -34,6 +41,8 @@ def runGame():
 	firedBullet = None
 	score = 0
 	blity = 630
+	playerHealth = 250
+
 	while done==False:
 		time_passed = pygame.time.get_ticks()/1000
 		pressed_keys = pygame.key.get_pressed()
@@ -61,8 +70,8 @@ def runGame():
 		if pressed_keys[K_DOWN]:
 			firedBullet = bullets.pop()
 
-		background1.checkbounds()
-		background1.draw_rotated(screen, background1.background_x, background1.background_y, score)
+		background1.checkbounds(screen, background1.background_x, background1.background_y)
+		#background1.draw_rotated(screen, background1.background_x, background1.background_y, score)
 
 		#draws enemy 
 		if enemy != None:
@@ -70,22 +79,24 @@ def runGame():
 			if screen_rect.contains(enemy_rect):
 				enemy.sizex += 1
 				enemy.sizey += 1
-				#enemy_rect = enemy.draw(screen, (enemy.x, enemy.y), enemy.sizex, enemy.sizey)
-
-			
+			#Reduce health if enemy hits player plane
+			if enemy.sizex >= 400:
+				playerHealth -= 42
+				enemy = None
+				enemy_rect = None
+				blity = 630
+				enemy = Enemy()
+				if playerHealth <= 0:
+					gameOver()
 
 		#Creates, scales and fires a bullet, ensures the bullets list remains full. 
 		if firedBullet != None:
-			#scaledBullet = pygame.transform.scale(firedBullet, (bulletsizex, bulletsizey))
-			scaledBullet = firedBullet
-			bullet_rect = screen.blit(scaledBullet, (SCREEN_SIZE[0]/2-10, bulletposy))
-			bulletposy -= 30
+			bullet_rect = screen.blit(firedBullet, (SCREEN_SIZE[0]/2-10, bulletposy))
+			bulletposy -= 20
 			bulletsizex -=1
 			bulletsizey -=1
 			if bulletposy < 400:
-				bulletposy = SCREEN_SIZE[1]
-				bulletsizex = 20
-				bulletsizey = 20
+				bulletposy = SCREEN_SIZE[1] - 100
 				while len(bullets) < 50:
 					bullets.append(firedBullet)
 				firedBullet = None
@@ -100,6 +111,7 @@ def runGame():
 						score += 1
 						enemy = Enemy()
 						enemy.health = randint(5, 15)
+						blity = 630
 					bulletposy = SCREEN_SIZE[1]
 					bulletsizex = 20
 					bulletsizey = 20
@@ -107,10 +119,11 @@ def runGame():
 						bullets.append(firedBullet)
 					firedBullet = None
 
-
-		headsupdisplay.draw(screen, angle)
+		headsupdisplay.drawcircle(screen, angle)
+		#Draw enemy blit
 		if enemy != None:
 			blitx = enemy.x/10 + 580
+			blity = enemy.sizex/4 + 620
 			if blitx > 723:
 				blitx = 723
 			if blitx < 557:
@@ -118,10 +131,10 @@ def runGame():
 			if blity >= 712:
 				blity = 712
 			enemyblit = pygame.draw.circle(screen, (255,0,0), (blitx, blity), 4, 0)
-		if enemy == None:
-			blity = 630
-		blity += 1
 		angle = headsupdisplay.updateangle(angle)
+		headsupdisplay.drawheadsup(screen)
+		headsupdisplay.drawHealth(screen, playerHealth)
+		draw_score(screen, score)
 		clock.tick(FPS)
 		pygame.display.update()
 
